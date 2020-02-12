3bd1a3ad7f55b33d21f3b35e6cbbf334d5948486
Matt Richardson <matt.richardson@octopus.com>
2020-02-12 13:58:07 +1100

    Update casing of class name

--- octopuscli.rb
+++ octopuscli.rb
@@ -1,4 +1,4 @@
-class OctopusCli < Formula
+class Octopuscli < Formula
   desc "The Octopus CLI (octo) for Octopus, a user-friendly DevOps tool for developers that supports release management, deployment automation, and operations runbooks"
   homepage "https://github.com/OctopusDeploy/OctopusCLI"
   url "https://octopus-downloads.s3-eu-west-1.amazonaws.com/octopus-tools/6.17.2/OctopusTools.6.17.2.osx-x64.tar.gz"
