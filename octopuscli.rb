e7b35960f5380f6bf6e74359b7912e4b9f111ca4
Matt Richardson <matt.richardson@octopus.com>
2020-02-12 13:59:40 +1100

    Lowercase the sha

--- octopuscli.rb
+++ octopuscli.rb
@@ -2,7 +2,7 @@ class Octopuscli < Formula
   desc "The Octopus CLI (octo) for Octopus, a user-friendly DevOps tool for developers that supports release management, deployment automation, and operations runbooks"
   homepage "https://github.com/OctopusDeploy/OctopusCLI"
   url "https://octopus-downloads.s3-eu-west-1.amazonaws.com/octopus-tools/6.17.2/OctopusTools.6.17.2.osx-x64.tar.gz"
-  sha256 "BA190CB25A6956172BC391F0616E9547C3723A5E0017FDF39F1E5F5271E0233A"
+  sha256 "ba190cb25a6956172bc391f0616e9547c3723a5e0017fdf39f1e5f5271e0233a"
 
   bottle :unneeded
 
