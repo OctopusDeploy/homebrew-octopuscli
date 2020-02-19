bcf208b029bf6a0b3083cd00232b362eb2c55703
OctoBob <support@octopus.com>
2020-02-19 05:26:45 +0000

    Update for release 7.0.1-enh-createlinuxpac20

--- octopuscli.rb
+++ octopuscli.rb
@@ -1,8 +1,8 @@
 class Octopuscli < Formula
   desc "The Octopus CLI (octo) for Octopus, a user-friendly DevOps tool for developers that supports release management, deployment automation, and operations runbooks"
   homepage "https://github.com/OctopusDeploy/OctopusCLI"
-  url "https://octopus-downloads.s3-eu-west-1.amazonaws.com/octopus-tools/7.0.1-enh-createlinuxpac18/OctopusTools.7.0.1-enh-createlinuxpac18.osx-x64.tar.gz"
-  sha256 "29e8fbbfa605700339812b111cb6cb8668f2a62134e54c4b6566e4a7d77a8a21"
+  url "https://octopus-downloads.s3-eu-west-1.amazonaws.com/octopus-tools/7.0.1-enh-createlinuxpac20/OctopusTools.7.0.1-enh-createlinuxpac20.osx-x64.tar.gz"
+  sha256 "eb7c9b22cbf56b57ac951b95c8922eac4093fc4c9f1f0c6b5d692977be6170fb"
 
   bottle :unneeded
 
