6736fa72f0f794655c835ad16e9af84a1fce2f2d
Matt Richardson <matt.richardson@octopus.com>
2020-02-20 13:38:05 +1100

    Accidentally published some pre-releases

--- octopuscli.rb
+++ octopuscli.rb
@@ -1,8 +1,8 @@
 class Octopuscli < Formula
   desc "The Octopus CLI (octo) for Octopus, a user-friendly DevOps tool for developers that supports release management, deployment automation, and operations runbooks"
   homepage "https://github.com/OctopusDeploy/OctopusCLI"
-  url "https://octopus-downloads.s3-eu-west-1.amazonaws.com/octopus-tools/7.0.1-ci8/OctopusTools.7.0.1-ci8.osx-x64.tar.gz"
-  sha256 "9f02cbd29862d578e311a76f9869d6d0c5ebef5eb72482fcdc36fc19dc071b88"
+  url "https://octopus-downloads.s3-eu-west-1.amazonaws.com/octopus-tools/7.0.0/OctopusTools.7.0.0.osx-x64.tar.gz"
+  sha256 "fa3f5194d35dbaceabb6577630b525041eb2a12643fc971164f995de04d1e24c"
 
   bottle :unneeded
 
