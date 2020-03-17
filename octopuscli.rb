009319a922aac1577a2d510f0773c318fe3a88a2
Thomas Spencer <tspencer@thegeneral.com>
2020-03-16 22:07:11 -0500

    Add version to formula

--- octopuscli.rb
+++ octopuscli.rb
@@ -1,6 +1,7 @@
 class Octopuscli < Formula
   desc "The Octopus CLI (octo) for Octopus, a user-friendly DevOps tool for developers that supports release management, deployment automation, and operations runbooks"
   homepage "https://github.com/OctopusDeploy/OctopusCLI"
+  version "7.2.0"
   url "https://octopus-downloads.s3-eu-west-1.amazonaws.com/octopus-tools/7.2.0/OctopusTools.7.2.0.osx-x64.tar.gz"
   sha256 "4fdc3b4caf707d121ffd359f7ee18685645395ab18d443410001b7f0a7bee19d"
 
