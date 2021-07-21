46ae866b8e795364f99d7f230a00552c134eb4d7
OctoBob <support@octopus.com>
2021-07-21 20:57:22 +0000

    Update for release 7.4.3320

--- octopuscli.rb
+++ octopuscli.rb
@@ -1,9 +1,9 @@
 class Octopuscli < Formula
   desc "The Octopus CLI (octo) for Octopus, a user-friendly DevOps tool for developers that supports release management, deployment automation, and operations runbooks"
   homepage "https://github.com/OctopusDeploy/OctopusCLI"
-  version "7.4.3319"
-  url "https://octopus-downloads.s3-eu-west-1.amazonaws.com/octopus-tools/7.4.3319/OctopusTools.7.4.3319.osx-x64.tar.gz"
-  sha256 "4a45b1091489ab97984968d7a4547effcf063c372565f6d99e17534684421c7b"
+  version "7.4.3320"
+  url "https://octopus-downloads.s3-eu-west-1.amazonaws.com/octopus-tools/7.4.3320/OctopusTools.7.4.3320.osx-x64.tar.gz"
+  sha256 "073ee9503363303321b11fd0ab432af177b510f93f8489d4e86eb744dadc6551"
 
   bottle :unneeded
 
